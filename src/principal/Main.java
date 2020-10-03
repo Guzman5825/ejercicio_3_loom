021805b9c876daed2506dc8742001c5930eb4d45
Edilberto <guzmanedi@hotmail.com>
2020-10-03 09:09:24 -0300

    modificacion del main

--- src/principal/Main.java
+++ src/principal/Main.java
@@ -5,7 +5,8 @@ import monjeVikingo.*;
 public class Main {
 
 	public static void main(String[] args) {///100 es la vida inicial
-		MonjeVikingo2 jugador= new MonjeVikingo2(2000);
+		
+MonjeVikingo1 jugador= new MonjeVikingo1(100);
 		
 		System.out.println(jugador);
 
@@ -44,6 +45,48 @@ public class Main {
 		jugador.recibirDaño(1000);
 		
 		System.out.println(jugador);
+		
+		System.out.println("-------------------------");
+		
+		MonjeVikingo2 jugador2= new MonjeVikingo2(2000);
+		
+		System.out.println(jugador2);
+
+		jugador2.meditar();
+		
+		System.out.println(jugador2);
+
+		jugador2.atacar();	///atacar tiene un daño base de 10(lo dice el enunciado)
+		
+		System.out.println(jugador2);
+
+		jugador2.recibirDaño(5);   ///necesita ingredar la cantidad de daño
+		
+		System.out.println(jugador2);
+		
+		jugador2.recibirDaño(20);
+		
+		System.out.println(jugador2);
+		
+		jugador2.atacar();
+		
+		System.out.println(jugador2);
+		
+		jugador2.recibirDaño(20);
+		
+		System.out.println(jugador2);
+		
+		jugador2.meditar();
+		
+		System.out.println(jugador2);
+		
+		jugador2.meditar();
+		
+		System.out.println(jugador2);
+		
+		jugador2.recibirDaño(1000);
+		
+		System.out.println(jugador2);
 	}
 
 }
